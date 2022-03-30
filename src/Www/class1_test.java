67159b90967d71441ac25efc42cfcf885844ef87
name2 <a.b@c.com>
2022-03-29 22:05:38 -0400

    first commit

--- src/Www/class1_test.java
+++ src/Www/class1_test.java
@@ -3,5 +3,6 @@ package Www;
 public class class1_test {
     public static void main(String[] args) {
         System.out.println("we are testing");
+        System.out.println("we will loop");
     }
 }
